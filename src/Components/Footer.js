0d95a11efadaeef7ab34313d7773891818d45b9a
sanketsapatevnit@gmail.com <sanketsapatevnit@gmail.com>
2023-05-07 17:56:29 +0530

    Changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = ()=>{
-    return <div className="flex absolute w-full justify-center h-7 bg-[#172337] text-white">
+    return <div className="flex text-sm align-middle absolute w-full justify-center pt-1 h-6 sm:h-7 bg-[#172337] text-white">
         © 2023 VNIT, Nagpur <span className="mx-4">|</span>  <a rel="noreferrer" target="_blank" href="https://sanket-sapate.github.io/" >Creator - Sanket Sapate</a>
     </div>
 }
--- src/Components/Model.js
+++ src/Components/Model.js
@@ -1,7 +1,26 @@
+import Spline from "@splinetool/react-spline";
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const Model = ()=>{
-    return (<></>)
+    const {department,topic} = useParams()
+    const obj = useSelector((storedata)=>storedata[department]?.model3d[topic])
+    return (<div className="mt-5 min-h-[79vh]">
+        <div>
+            <div className="text-center sm:text-left text-xl font-bold">
+                {obj?.title}
+            </div>
+            <div className="sm:flex sm:justify-around  pb-5" >
+                <div className="py-5 h-[250px] sm:mt-10 sm:h-[70vh] sm:w-[40vw]">
+                    <Spline scene={obj?.link}/>
+                </div>
+                <div className="sm:w-1/2">
+                    {obj?.description.map((e)=><div className="pt-5 text-justify">{e}</div>)}
+                </div>
+            </div>
+        </div>
+    </div>)
 }
 
 export default Model
