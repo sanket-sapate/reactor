cc96c713162c20a352ba997cff32f227e3e9e4d9
sanketsapatevnit@gmail.com <sanketsapatevnit@gmail.com>
2023-08-05 18:05:59 +0530

    Reduced bundle sizes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { lazy,Suspense } from 'react';
 import AllRoutes from './Components/AllRoutes';
 import { ToastContainer} from 'react-toastify';
 import { Routes,Route, useLocation } from 'react-router-dom';
 import { isLogin } from './Redux/action';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import Login from './Components/Login';
 import IsLogged from './Components/PrivateRoutes/IsLogged';
-import Profile from './Components/Profile';
 import IsNotLogged from './Components/PrivateRoutes/IsNotLogged';
 import {AnimatePresence,motion} from 'framer-motion'
+import LoadingComp from './Components/LoadingComp';
+const Profile =lazy(()=>import ('./Components/Profile'));
+const Login = lazy(()=>import ('./Components/Login'));
 
 function App() {
   const location = useLocation()
@@ -53,7 +55,9 @@ function App() {
                   transition={{ duration: .7  }}
                   className='w-full h-full absolute min-h-screen bg-white'
                   >
-                    <Profile/>
+                    <Suspense fallback={<LoadingComp/>}>
+                      <Profile/>
+                    </Suspense>
                   </motion.div>
               } />
               <Route path='/user/:section' element={<motion.div
@@ -63,7 +67,9 @@ function App() {
                   exit='exit'
                   transition={{ duration: .7  }}
                   className='w-full h-full absolute min-h-screen bg-white'
-                  ><IsNotLogged to='/user'><Profile/></IsNotLogged>
+                  ><IsNotLogged to='/user'><Suspense fallback={<LoadingComp/>}>
+                  <Profile/>
+                </Suspense></IsNotLogged>
                   </motion.div>
                   } />
               <Route path='/user/setting/:settingsection' element={<motion.div
@@ -73,7 +79,9 @@ function App() {
                   exit='exit'
                   transition={{ duration: .7  }}
                   className='w-full h-full absolute min-h-screen bg-white'
-                  ><IsNotLogged to='/user'><Profile/></IsNotLogged>
+                  ><IsNotLogged to='/user'><Suspense fallback={<LoadingComp/>}>
+                  <Profile/>
+                </Suspense></IsNotLogged>
               </motion.div>
               } />
               <Route path='*' element={<motion.div
@@ -84,7 +92,7 @@ function App() {
                   >
                     <AllRoutes/>
                   </motion.div>} />
-              <Route path='/signin' element={<IsLogged to='/'><Login/></IsLogged>} />
+              <Route path='/signin' element={<IsLogged to='/'><Suspense fallback={LoadingComp}><Login/></Suspense></IsLogged>} />
             </Routes>
         </AnimatePresence>
     </>
