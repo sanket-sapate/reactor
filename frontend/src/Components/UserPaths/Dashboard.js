a56378c1c4e0083501ff81b5beb56aef2cba67c8
sanketsapatevnit@gmail.com <sanketsapatevnit@gmail.com>
2023-08-28 17:45:49 +0530

    Dashboard updated

--- frontend/src/Components/UserPaths/Dashboard.js
+++ frontend/src/Components/UserPaths/Dashboard.js
@@ -4,7 +4,22 @@ import CanvasJSReact from '@canvasjs/react-charts';
 // var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-
+const randomArray = (length) => {
+    var array = [];
+    var num = 100
+    for (var i = 0; i < length; i++) {
+        if(i<length/2){
+            num += Math.floor(Math.random() * 5) - Math.floor(Math.random() * 5);
+        }else{
+            num += Math.floor(Math.random() * 5) - Math.floor(Math.random() * 4);
+        }
+        if(num<100){
+            num = 101
+        }
+        array.push({y:num,x:new Date(2021,5,i)});
+    }
+    return array;
+}
 export default function Dashboard(){
     const options = {
         animationEnabled: true,
@@ -15,48 +30,59 @@ export default function Dashboard(){
         toolTip: {
             shared: true
         },
+        axisY: {
+            gridThickness: 0,
+            tickLength: 0,
+            lineThickness: 0,
+            labelFormatter: function(){
+            return " ";
+            }
+        },
+        axisY2: {
+            gridThickness: 0,
+            tickLength: 0,
+            lineThickness: 0,
+            labelFormatter: function(){
+                return " ";
+            }
+        },
+        axisX:{
+            gridThickness: 0,
+            tickLength: -6,
+            labelFormatter:function(){
+                return "";
+            }
+        },
         data: [{
             type: "spline",
             name: "Profile Visits",
-            dataPoints: [
-                { y: 155, label: "Jan" },
-                { y: 150, label: "Feb" },
-                { y: 152, label: "Mar" },
-                { y: 148, label: "Apr" },
-                { y: 142, label: "May" },
-                { y: 150, label: "Jun" },
-                { y: 146, label: "Jul" },
-                { y: 149, label: "Aug" },
-                { y: 153, label: "Sept" },
-                { y: 158, label: "Oct" },
-                { y: 154, label: "Nov" },
-                { y: 150, label: "Dec" }
-            ]
-        },
-        {
-            type: "spline",
-            name: "Views",
-            axisYType: "secondary",
-            dataPoints: [
-                { y: 172, label: "Jan" },
-                { y: 173, label: "Feb" },
-                { y: 175, label: "Mar" },
-                { y: 172, label: "Apr" },
-                { y: 162, label: "May" },
-                { y: 165, label: "Jun" },
-                { y: 172, label: "Jul" },
-                { y: 168, label: "Aug" },
-                { y: 175, label: "Sept" },
-                { y: 170, label: "Oct" },
-                { y: 165, label: "Nov" },
-                { y: 169, label: "Dec" }
-            ]
-		}]
+            dataPoints: randomArray(500)
+        }],
+        zoomEnabled: true,
+        height: 300,
 	}
     return <>
     <div className="grid md:grid-cols-2 gap-5 grid-rows-1 ">
         <div className="py-5 px-2 rounded-xl shadow-2xl ">
             <CanvasJSChart options = {options}/>
+        </div>
+        <div className="px-2 grid grid-cols-2 grid-rows-2 gap-3 h-64 md:h-auto ">
+            <div className="flex flex-col items-center justify-center rounded-xl shadow-xl">
+                <p className="text-2xl font-bold text-gray-900">Upvotes</p>
+                <h1 className="text-2xl font-bold text-gray-900">100</h1>
+            </div>
+            <div className="flex flex-col  items-center justify-center rounded-xl shadow-xl">
+                <p className="text-2xl font-bold text-gray-900">DownVotes</p>
+                <h1 className="text-2xl font-bold text-gray-900">10</h1>
+            </div>
+            <div className="flex flex-col  items-center justify-center rounded-xl shadow-xl">
+                <p className="text-2xl font-bold text-gray-900">Shares</p>
+                <h1 className="text-2xl font-bold text-gray-900">100</h1>
+            </div>
+            <div className="flex flex-col  items-center justify-center rounded-xl shadow-xl">
+                <p className="text-2xl font-bold text-gray-900">Total Views</p>
+                <h1 className="text-2xl font-bold text-gray-900">100</h1>
+            </div>
         </div>
     </div>
     </>
